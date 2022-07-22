bb9d6e469626471e2896aba3ddf749c1a58ec14f
Derek Douglas <derek.j.douglas13@gmail.com>
2022-07-21 20:33:47 -0500

    begins making collection class for lab 04

---
+++ src/models/collection.js
@@ -0,0 +1,52 @@
+const { throws } = require("assert");
+
+class Collection {
+  constructor(model, app, route) {
+    this.model = model;
+    this.route = route;
+  }
+
+  async create(req, res) {
+    const construct = this.model.build(req.body);
+    await construct.save;
+    res.status(200).send(construct);
+  }
+
+  async readOne(req, res) {
+    const read = await this.model.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (read.length > 0) {
+      res.status(200).send(read[0]);
+    } else {
+      res.status(400).send(`Could not find file with id ${req.params.id}`);
+    }
+  }
+
+  async readMany(req, res) {
+    const list = await this.model.findAll();
+    res.status(200).send(list);
+  }
+
+  // async update(req, res) {
+  //   const updateFile = await this.model.findAll({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   })
+  // }
+  async delete(req, res) {
+    const deleteFile = await this.model.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deleteFile.length > 0) {
+      res.status(200).send('Error');
+    } else {
+      res.status(200).send('Item Deleted');
+    }
+  }
+}
